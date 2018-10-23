253c8bfbbd9efd5972fb31fe4de9bd5a42882ae7
web100ry <valerij.ivanjuk@gmail.com>
2018-10-23 22:53:17 +0300

    Test1 from Week3

---
+++ Week3/Test1/index.js
@@ -0,0 +1,9 @@
+/**
+ * @param {String} date
+ * @returns {Object}
+ */
+module.exports = function (date) {
+value: 'test';
+    return (date);
+
+};
--- Week3/Test1/test.js
+++ Week3/Test1/test.js
@@ -4,9 +4,11 @@ var tweet={text:'#test blabla #testtest'};
 Object.defineProperty(tweet,'linkify',{
     get: function () {
         return this.text.replace(
-            /#[a-z0-9]+/gi, '<a href="$1">$1</a>'
+            /(#[a-z0-9]+)/gi, '<a href="$1">$1</a>'
         );
     }
 });
 test = Object.getOwnPropertyDescriptors(tweet, 'linkify');
-console.info(test);
+test2 = tweet.linkify;
+
+console.info(test2);
